764165c44cdecfe7c1c5f921a50b1cd496c3aa5d
Reddysai <reddysai@agiratech.com>
2017-08-09 18:40:38 +0530

    updated attributes

--- ruby-install/attributes/default.rb
+++ ruby-install/attributes/default.rb
@@ -1,4 +1,11 @@
 default["ruby-install"]["git_url"] = "https://github.com/postmodern/ruby-install.git"
-default["ruby-install"]["git_ref"] = "v0.5.0"
-default["ruby-install"]["install_path"] = nil
-default["ruby-install"]["rubies"] = []
+default["ruby-install"]["git_ref"] = "v0.6.1"
+default["ruby-install"]["install_path"] = "/opt/ruby-install"
+default["ruby-install"]["rubies"] = [
+      {
+        ruby: "ruby 2.4.1",
+        user: "root",
+        group: "root",
+        reinstall: true
+      }
+    ]
