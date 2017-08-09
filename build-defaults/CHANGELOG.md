4245fbd305b849d499e6293091ce3cb34f494b58
Reddysai <reddysai@agiratech.com>
2017-08-09 20:03:44 +0530

    added build defaults

---
+++ build-defaults/CHANGELOG.md
@@ -0,0 +1,11 @@
+# build-defaults CHANGELOG
+
+This file is used to list changes made in each version of the build-defaults cookbook.
+
+## 0.1.0
+- [your_name] - Initial release of build-defaults
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ build-defaults/recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: build-defaults
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+case node['platform']
+when "centos"
+    execute 'update' do
+      command 'yum update -y'
+    end
+
+    execute 'wget-tar' do
+      command 'yum install wget -y && yum install tar -y'
+    end
+
+    execute 'psmisc' do
+      command 'yum install psmisc -y'
+    end
+
+    execute 'groupinstall' do
+      command "yum groupinstall 'Development Tools' -y"
+    end
+
+    execute 'epel-release' do
+      command 'yum install epel-release -y'
+    end
+when "ubuntu"
+    execute 'update' do
+      command 'apt-get update -y'
+    end
+
+    execute 'wget-tar' do
+      command 'apt-get install wget -y && apt-get install tar -y'
+    end
+
+    execute 'psmisc' do
+      command 'apt-get install psmisc -y'
+    end
+end
+
+
+
