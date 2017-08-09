305e7f842f364adeeb5cbc3d6bae9a5f6192d3b8
Reddysai <reddysai@agiratech.com>
2017-08-09 17:11:37 +0530

    updated version

--- rubyenv/attributes/default.rb
+++ rubyenv/attributes/default.rb
@@ -1,7 +1,5 @@
 # rubocop:disable Style/LeadingCommentSpace
 # Default recipe
 default["rubyenv"]["rubies"] = [
-  '1.9.3-p551' => false,
-  '2.1.5' => false,
-  '2.2.3' => true
+  '2.4.1' => true
 ]
