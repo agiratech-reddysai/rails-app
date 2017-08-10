e717b90dbe18844939d2099450c79d62985b5d67
Reddysai <reddysai@agiratech.com>
2017-08-10 19:41:45 +0530

    changed version

--- mysql-replication/recipes/default.rb
+++ mysql-replication/recipes/default.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 mysql_service 'foo' do
   port '3306'
-  version '5.5'
-  initial_root_password 'change me'
+  version '5.7'
+  initial_root_password 'change_me'
   action [:create, :start]
 end
