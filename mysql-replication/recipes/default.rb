8026833e01c4b161c25f57c72b92d5d11a460df9
Reddysai <reddysai@agiratech.com>
2017-08-11 11:29:36 +0530

    added port

--- mysql-replication/recipes/default.rb
+++ mysql-replication/recipes/default.rb
@@ -26,6 +26,8 @@ end
 # Configure the MySQL service
 mysql_service 'default' do
   version '5.7'
+  bind_address '0.0.0.0'
+  port '3306'
   initial_root_password 'root'
   action [:create, :start]
 end
