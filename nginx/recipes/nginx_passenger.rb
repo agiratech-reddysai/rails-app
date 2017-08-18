09e1973a96c227d91235e0a1869a4f6e9562ecc6
Reddysai <reddysai@agiratech.com>
2017-08-18 14:39:41 +0530

    changed syntax

--- nginx/recipes/nginx_passenger.rb
+++ nginx/recipes/nginx_passenger.rb
@@ -1,56 +1,56 @@
 
 
-case node[:platform]
-when 'centos'
-  # execute 'add out e17 yum repo' do
-  #   command 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo'
-  # end
-  package 'nginx'
-  # package 'passenger'
-when 'ubuntu'
-  include_recipe 'apt'
-  package 'apt-transport-https'
+# case node[:platform]
+# when 'centos'
+#   execute 'add out e17 yum repo' do
+#     command 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo'
+#   end
+#   package 'nginx'
+#   package 'passenger'
+# when 'ubuntu'
+#   include_recipe 'apt'
+#   package 'apt-transport-https'
 
-  apt_uri = node.nginx_passenger.use_passenger_4 ? "https://oss-binaries.phusionpassenger.com/apt/passenger/4" : "https://oss-binaries.phusionpassenger.com/apt/passenger"
+#   apt_uri = node.nginx_passenger.use_passenger_4 ? "https://oss-binaries.phusionpassenger.com/apt/passenger/4" : "https://oss-binaries.phusionpassenger.com/apt/passenger"
 
-  apt_repository "phusion" do
-    action        :add
-    uri           apt_uri
-    distribution  node.lsb.codename
-    components    ['main']
-    keyserver     "hkp://keyserver.ubuntu.com:80"
-    key           "561F9B9CAC40B2F7"
-  end
+#   apt_repository "phusion" do
+#     action        :add
+#     uri           apt_uri
+#     distribution  node.lsb.codename
+#     components    ['main']
+#     keyserver     "hkp://keyserver.ubuntu.com:80"
+#     key           "561F9B9CAC40B2F7"
+#   end
 
-  package "nginx-common" do
-    options '-o DPkg::Options::="--force-confold"'
-  end
-  package 'nginx-extras'
-  package 'passenger'
-end
+#   package "nginx-common" do
+#     options '-o DPkg::Options::="--force-confold"'
+#   end
+#   package 'nginx-extras'
+#   package 'passenger'
+# end
 
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action   :enable
-end
+# service 'nginx' do
+#   supports :status => true, :restart => true, :reload => true
+#   action   :enable
+# end
 
-bash 'current_path' do
-  code <<-EOH
-    mkdir -p #{node['nginx']['current_path']}
-  EOH
-end
+# bash 'current_path' do
+#   code <<-EOH
+#     mkdir -p #{node['nginx']['current_path']}
+#   EOH
+# end
 
-include_recipe 'nginx::commons'
+# include_recipe 'nginx::commons'
 
-template "#{node['nginx']['dir']}/conf.d/passenger.conf" do
-  source 'modules/passenger1.conf.erb'
-  owner  'root'
-  group  node['root_group']
-  mode   '0644'
-  notifies :reload, 'service[nginx]', :delayed
-end
+# template "#{node['nginx']['dir']}/conf.d/passenger.conf" do
+#   source 'modules/passenger1.conf.erb'
+#   owner  'root'
+#   group  node['root_group']
+#   mode   '0644'
+#   notifies :reload, 'service[nginx]', :delayed
+# end
 
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action   :start
-end
+# service 'nginx' do
+#   supports :status => true, :restart => true, :reload => true
+#   action   :start
+# end
