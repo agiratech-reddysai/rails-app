46541971eee7b2cd2c7ee2a40080de646688f147
Reddysai <reddysai@agiratech.com>
2017-08-18 14:37:18 +0530

    changed syntax

--- nginx/recipes/nginx_passenger.rb
+++ nginx/recipes/nginx_passenger.rb
@@ -2,9 +2,9 @@
 
 case node[:platform]
 when 'centos'
-  execute 'add out e17 yum repo' do
-    command 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo'
-  end
+  # execute 'add out e17 yum repo' do
+  #   command 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo'
+  # end
   package 'nginx'
   # package 'passenger'
 when 'ubuntu'
