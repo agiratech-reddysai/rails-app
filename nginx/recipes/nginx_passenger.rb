9251c97ea23784004e1804866913066647bddb27
Reddysai <reddysai@agiratech.com>
2017-08-21 13:03:03 +0530

    added package curl

--- nginx/recipes/nginx_passenger.rb
+++ nginx/recipes/nginx_passenger.rb
@@ -2,6 +2,7 @@
 
 case node[:platform]
 when 'centos'
+  package 'curl'
   execute 'add out e17 yum repo' do
     command 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo'
   end
